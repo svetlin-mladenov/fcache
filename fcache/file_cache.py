4516f5fbbe078472d1dbbcbb0f93720b81f7c051
Svetlin Mladenov <svetlin@ai.hacker.works>
2017-04-20 07:34:07 +0000

    #6: On disk cache is limited to 80G

--- fcache/file_cache.py
+++ fcache/file_cache.py
@@ -2,24 +2,64 @@ import os
 import pickle
 
 
+# TODO use slots
+class LruItem:
+    def __init__(self, prev, next, key):
+        self.prev = prev
+        self.next = next
+        self.key = key
+
+# TODO use slots
+class LruList:
+    def __init__(self, next):
+        self.next = next
+
 class FileCache:
-    def __init__(self, cache_dir):
+    def __init__(self, cache_dir, capacity=80*1024*1024*1024):
         self._cache_dir = cache_dir
-        self._cached_keys = set()
+        self._cached_keys = dict()
+        self._capacity = capacity
+        self._lru_items = LruList(None)  # Dummy item in order to make popping easier
+        self._mru_item = self._lru_items
+        self._fill = 0
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)
+        disk_files = []
         for cached_pickle in os.listdir(cache_dir):
             key, ext = os.path.splitext(cached_pickle)
             if ext != '.pkl': continue
-            self._cached_keys.add(int(key))
+            key = int(key)
+            full_fn = os.path.join(cache_dir, cached_pickle)
+            disk_files.append((full_fn, os.stat(full_fn), key))
+        for _, stat, key in sorted(disk_files, key=lambda r: r[1].st_atime_ns):
+            self._fill += stat.st_size
+            self._cached_keys[key] = self.insert_mru(key)
 
     def __setitem__(self, key, value):
         cache_fn = self.cache_fn(key)
         with open(cache_fn, 'wb') as f:
             pickle.dump(value, f)
-        self._cached_keys.add(key)
+            self._fill += f.tell()
+        self._cached_keys[key] = self.insert_mru(key)
+        while self._fill > self._capacity:
+            self.pop_lru()
+
+    def insert_mru(self, key):
+        lru_item = LruItem(self._mru_item, None, key)
+        self._mru_item.next = lru_item
+        self._mru_item = lru_item
+        return lru_item
 
     def __getitem__(self, key):
+        lru_item = self._cached_keys[key]
+        if lru_item is not self._mru_item:
+            lru_item.prev.next = lru_item.next
+            if lru_item.next is not None:
+                lru_item.next.prev = lru_item.prev
+            self._mru_item.next = lru_item
+            lru_item.prev = self._mru_item
+            lru_item.next = None
+            self._mru_item = lru_item
         cache_fn = self.cache_fn(key)
         try:
             with open(cache_fn, 'rb') as f:
@@ -32,6 +72,18 @@ class FileCache:
 
     def __contains__(self, key):
         return key in self._cached_keys
+
+    def pop_lru(self):
+        lru = self._lru_items.next
+        #del self[lru.key]
+        del self._cached_keys[lru.key]
+        size = os.stat(self.cache_fn(lru.key)).st_size
+        os.unlink(self.cache_fn(lru.key))
+        lru.prev.next = lru.next
+        if lru.next is not None:
+            lru.next.prev = lru.prev
+        lru.next = None  # no dangling references
+        self._fill -= size
 
     def clear(self):
         self._cached_keys = set()
--- fcache/tests/test_file_cache.py
+++ fcache/tests/test_file_cache.py
@@ -18,6 +18,7 @@ def test_caching_values(tmpdir):
         assert cache[k] == v
     assert len(cache) == 3
 
+
 def test_cache_clearing(tmpdir):
     cache = FileCache(str(tmpdir))
     kvs = [(1, 1), (2, 20), (3, 300)]
@@ -26,3 +27,36 @@ def test_cache_clearing(tmpdir):
     assert len(cache) == 3
     cache.clear()
     assert len(cache) == 0
+
+def test_cache_limit(tmpdir):
+    record_size = 512
+    cache = FileCache(str(tmpdir), capacity=int(2.5*record_size))
+    cache[0] = bytes(record_size)
+    cache[1] = bytes(record_size)
+    cache[2] = bytes(record_size)
+    assert 0 not in cache  # 0 is the LRU
+    assert 1 in cache
+    assert 2 in cache
+
+    assert cache[1] is not None  # make 2 the LRU
+    cache[3] = bytes(record_size)
+    assert 0 not in cache
+    assert 2 not in cache
+    assert 1 in cache
+    assert 3 in cache
+
+def test_cache_limit_restore_from_disk(tmpdir):
+    record_size = 512
+    cache = FileCache(str(tmpdir), capacity=int(2.5*record_size))
+    cache[0] = bytes(record_size)
+    cache[1] = bytes(record_size)
+    cache[2] = bytes(record_size)
+
+    del cache
+    cache = FileCache(str(tmpdir), capacity=int(2.5*record_size))
+    assert 0 not in cache
+    assert 1 in cache
+    assert 2 in cache
+
+    cache[3] = bytes(record_size)
+    assert 1 not in cache
